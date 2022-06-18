2469bf835e6e8a1ac9277985a3ed22f1fc4afefc
Valeriia <volikavs@gmail.com>
2022-06-18 23:00:22 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
+
 import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframe);
+//  CurrentTime to localStorage
+player.on('timeupdate', throttle(onPlayCurrentTime, 1000));
+
+function onPlayCurrentTime({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
