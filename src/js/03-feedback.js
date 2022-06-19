b25453b40ffe1d39d1a69673880b273329b52d3a
Valeriia <volikavs@gmail.com>
2022-06-19 02:01:23 +0200

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state'
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let formInput = {};
+
+currentFormData();
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    // console.log('Submit form');
+
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(evt) {
+  formInput[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formInput));
+    //  console.log(formInput);
+}
+
+function currentFormData() {
+    const formInputCurrent = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (formInputCurrent) {
+        const email = document.querySelector('.feedback-form input');
+        const message = document.querySelector('.feedback-form textarea');
+        email.value = formInputCurrent.email;
+        message.value = formInputCurrent.message;     
+        console.log(formInputCurrent);
+    }
+
+}
