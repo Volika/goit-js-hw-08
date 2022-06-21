495be197cd64e17e1391ffae586700bbd5a22a77
Valeriia <volikavs@gmail.com>
2022-06-22 00:04:18 +0200

    update 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,34 +8,42 @@ form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
 let formInput = {
-    "email": '',
-    "message": '',
+    // "email": '',
+    // "message": '',
 };
 
 currentFormData();
 
 function onFormSubmit(evt) {
+    const formInputCurrent = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    console.log(formInputCurrent);
     evt.preventDefault();
-    // console.log('Submit form');
-
+  
     evt.currentTarget.reset();
+ 
     localStorage.removeItem(STORAGE_KEY);
+    formInput = {};
+
 }
 
 function onFormInput(evt) {
   formInput[evt.target.name] = evt.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formInput));
-    //  console.log(formInput);
 }
 
 function currentFormData() {
     const formInputCurrent = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    if (formInputCurrent) {
-        const email = document.querySelector('.feedback-form input');
-        const message = document.querySelector('.feedback-form textarea');
+    const email = document.querySelector('.feedback-form input');
+    const message = document.querySelector('.feedback-form textarea');
+    
+    if ("email" in formInputCurrent) {
         email.value = formInputCurrent.email;
-        message.value = formInputCurrent.message;     
-        console.log(formInputCurrent);
     }
+   if ("message" in formInputCurrent) {
+        message.value = formInputCurrent.message;
+    }
+
+    // email.value =  ? formInputCurrent.email : '';
+    // message.value = formInputCurrent.message ? formInputCurrent.message : '';
 
 }
